80252f5ecabfb28923b91b2338516a4f6bdf8d55
lim0n3 <simonestaffa96@gmail.com>
2024-03-16 12:18:16 +0000

    zerodev integration

--- components/shared/providers.tsx
+++ components/shared/providers.tsx
@@ -9,6 +9,7 @@ import { createConfig, WagmiProvider } from "wagmi";
 import { http } from "viem";
 import { base } from "viem/chains";
 import { FluidkeyClient, FluidkeyProvider } from "@sefu/react-sdk";
+import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
 
 const sefuClient = new FluidkeyClient({
   clientId: process.env.NEXT_PUBLIC_FLUIDKEY_APP_ID!,
@@ -37,7 +38,10 @@ export default function Providers(props: { children: ReactNode }) {
       <DynamicContextProvider
         settings={{
           environmentId,
-          walletConnectors: [EthereumWalletConnectors],
+          walletConnectors: [
+            EthereumWalletConnectors,
+            ZeroDevSmartWalletConnectors,
+          ],
           //   hideEmbeddedWalletTransactionUIs: true,
         }}
       >
