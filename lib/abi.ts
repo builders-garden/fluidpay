076a5bf72006d2fd7ffb00fdde504dca46229eb1
lim0n3 <simonestaffa96@gmail.com>
2024-03-16 00:50:00 +0000

    deploy fluidkey stealth address

---
+++ lib/abi.ts
@@ -0,0 +1,11 @@
+export const FLUIDKEY_HYDRATOR_ABI = [
+  {
+    inputs: [
+      { internalType: "bytes32", name: "signerAddress", type: "bytes32" },
+    ],
+    name: "deploySafe",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
