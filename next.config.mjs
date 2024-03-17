1c88653489bc0493fecad5daf5f3fd84ddde8481
PaoloRollo <paolo.rollo1997@gmail.com>
2024-03-17 04:46:25 +0000

    feat: UI fixes.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
